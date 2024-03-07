eec1af46bb4957f2e5bed30532bea1d9e491c1ba
mehediFlutter <mehedii.edu.bd@gmail.com>
2024-03-07 20:41:07 +0600

    chatting font person widget created

--- lib/package/chatting/chat_font_screen.dart
+++ lib/package/chatting/chat_font_screen.dart
@@ -73,29 +73,48 @@ class _ChatFontScreenState extends State<ChatFontScreen> {
             ),
             SizedBox(height: size.height / 20),
             ListTile(
-              leading: Stack(
-                children: [
-                  Container(
-                      height: 45.05,
-                      width: 40,
-                      decoration: BoxDecoration(shape: BoxShape.circle),
-                      child: Image.asset('assets/images/chatting_image.png')),
-               
-                       Container(
-                        margin: EdgeInsets.only(left: 30,top: 26),
-                    height: 11,
-                    width: 11,
-                    decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.black,),
-                    child: Image.asset('assets/icons/green_tolu.png'),
-                   
-                  ),
-                 
-                ],
-              ),
+              leading: Chatting_profile_with_green_tolu(),
+           title: Text('Shah Alam'),
             ),
+            
           ],
         ),
       ),
     ));
+  }
+}
+
+class Chatting_profile_with_green_tolu extends StatefulWidget {
+  final bool isActive;
+  const Chatting_profile_with_green_tolu({
+    super.key, this.isActive=true,
+  });
+
+  @override
+  State<Chatting_profile_with_green_tolu> createState() => _Chatting_profile_with_green_toluState();
+}
+
+class _Chatting_profile_with_green_toluState extends State<Chatting_profile_with_green_tolu> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+       Container(
+            height: 45.05,
+            width: 40,
+            decoration: BoxDecoration(shape: BoxShape.circle),
+            child: Image.asset('assets/images/chatting_image.png')),
+     
+             widget.isActive? Container(
+              margin: EdgeInsets.only(left: 30,top: 26),
+          height: 11,
+          width: 11,
+          decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.black,),
+          child:  Image.asset('assets/icons/green_tolu.png'),
+         
+        ):SizedBox(),
+       
+      ],
+    );
   }
 }
