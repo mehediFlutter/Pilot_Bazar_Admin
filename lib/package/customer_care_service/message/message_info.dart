6478a5891c8e330fae160fd7fd6f4725e33c2a6f
mehediFlutter <mehedii.edu.bd@gmail.com>
2024-03-15 01:09:18 +0600

    15/3/2024

---
+++ lib/package/customer_care_service/message/message_info.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MessageInfoClass extends StatefulWidget {
+  const MessageInfoClass({super.key});
+
+  @override
+  State<MessageInfoClass> createState() => _MessageInfoClassState();
+}
+
+class _MessageInfoClassState extends State<MessageInfoClass> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
